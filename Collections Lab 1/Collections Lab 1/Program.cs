3f360bb4ec7bdb8a2117535922330244b421aeb3
Michael Smith <mikesmithnova@gmail.com>
2018-02-17 18:57:59 -0400

    Collections Lab 1 complete

---
+++ Collections Lab 1/Collections Lab 1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList myArrList = new ArrayList();
+            myArrList.Add("First Item");
+            myArrList.Add(3);
+            myArrList.Add(4.5);
+            foreach (object obj in myArrList)
+            {
+                Console.WriteLine(obj.ToString());
+            }
+
+            ArrayList myArrList2 = new ArrayList();
+            myArrList2.Add("First Item");
+            myArrList2.Add("Third Item");
+            myArrList2.Add("Second Item");
+
+            myArrList2.Sort();
+            int itemIndex = myArrList2.IndexOf("Third Item");
+
+            foreach (object obj in myArrList2)
+            {
+                Console.WriteLine(obj.ToString());
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Third Item is at index {itemIndex}.");
+        }
+    }
+}
