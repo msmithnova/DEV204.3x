dbf692050f5997cd241789e72df987064883d8a0
Michael Smith <mikesmithnova@gmail.com>
2018-02-17 21:24:34 -0400

    Generics Assessmet Lab complete

--- GenericsLab1/GenericsLab1/Program.cs
+++ GenericsLab1/GenericsLab1/Program.cs
@@ -17,14 +17,37 @@ namespace GenericsLab1
             Student stud2 = new Student("Fred", "Flintstone", 45, "Geology");
             Student stud3 = new Student("Mickey", "Mouse", 35, "Animation");
 
+            float[] grades = new float[] { 88, 92, 97, 83, 82, 97, 92, 88, 82, 95, 91, 98, 92, 94, 89 };
+            for (int i = 0; i < 5; i++)
+            {
+                stud1.Grades.Push(grades[i]);
+                stud2.Grades.Push(grades[i + 5]);
+                stud3.Grades.Push(grades[i + 10]);
+                stud1.GradesSet.Add(grades[i]);
+                stud2.GradesSet.Add(grades[i + 5]);
+                stud3.GradesSet.Add(grades[i + 10]);
+            }
+
             students.Add(stud1);
             students.Add(stud2);
             students.Add(stud3);
 
             foreach (Student stud in students)
             {
-                Console.WriteLine(stud.FirstName);
+                Console.WriteLine(stud.FirstName + " " + stud.LastName + " has grades of: ");
+                foreach (float grade in stud.GradesSet)
+                {
+                    Console.Write(grade + ", ");
+                }
+                Console.WriteLine();
             }
+
+            stud1.Grades.Pop();
+            stud1.Grades.Push(85);
+            stud2.Grades.Pop();
+            stud2.Grades.Push(97);
+            stud3.Grades.Pop();
+            stud3.Grades.Push(93);
 
             bool exists = students.Contains<Student>(stud1);
             Console.WriteLine(exists.ToString());
@@ -45,11 +68,15 @@ namespace GenericsLab1
             this.LastName = last;
             this.Age = age;
             this.Program = prog;
+            this.Grades = new Stack<float>();
+            this.GradesSet = new SortedSet<float>();
         }
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Age { get; set; }
         public string Program { get; set; }
+        public Stack<float> Grades { get; set; }
+        public SortedSet<float> GradesSet { get; set; }
     }
 }
